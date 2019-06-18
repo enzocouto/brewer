8c8b24803e2caea3d714d637c965df1a9294b920
Enzo Couto <enzocouto@gmail.com>
2019-06-18 02:45:35 -0300

    finalizando capitulo 18

--- src/main/java/br/com/ecouto/brewer/controller/UsuariosController.java
+++ src/main/java/br/com/ecouto/brewer/controller/UsuariosController.java
@@ -2,6 +2,7 @@ package br.com.ecouto.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,14 +11,25 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.ecouto.brewer.model.Usuario;
+import br.com.ecouto.brewer.repository.GrupoRepository;
+import br.com.ecouto.brewer.service.CadastroUsuarioService;
+import br.com.ecouto.brewer.service.exception.EmailUsuarioJaCadastradoException;
+import br.com.ecouto.brewer.service.exception.SenhaObrigatoriaUsuarioException;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuariosController {
 
+	@Autowired
+	CadastroUsuarioService cadastroUsuarioService;
+	
+	@Autowired
+	GrupoRepository grupoRepository;
+	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Usuario usuario) {
 		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
+		mv.addObject("grupos",grupoRepository.findAll());
 		return mv;
 	}
 	
@@ -27,7 +39,17 @@ public class UsuariosController {
 			return novo(usuario);
 		}
 		
+		try {
+			cadastroUsuarioService.salvar(usuario);
+		}catch (EmailUsuarioJaCadastradoException e) {
+			result.rejectValue("email", e.getMessage(),e.getMessage());
+			return novo(usuario);
+		}catch(SenhaObrigatoriaUsuarioException ex) {
+			result.rejectValue("senha", ex.getMessage(),ex.getMessage());
+			return novo(usuario);
+		}
 		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso");
+		usuario = new Usuario();
 		return new ModelAndView("redirect:/usuarios/novo");
 	}
 	
--- src/main/java/br/com/ecouto/brewer/model/Usuario.java
+++ src/main/java/br/com/ecouto/brewer/model/Usuario.java
@@ -15,6 +15,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
@@ -44,9 +45,10 @@ public class Usuario implements Serializable {
 	@Transient
 	private String confirmacaoSenha;
 
+	@NotNull(message="Status é obrigatório")
 	private Boolean ativo;
 
-	@NotNull(message = "Selecione pelo menos um grupo")
+	@Size(min=1 , message = "Selecione pelo menos um grupo")
 	@ManyToMany
 	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "codigo_usuario")
 				, inverseJoinColumns = @JoinColumn(name = "codigo_grupo"))	
@@ -58,6 +60,10 @@ public class Usuario implements Serializable {
 
 	public Long getCodigo() {
 		return codigo;
+	}
+	
+	public boolean isNovo() {
+		return codigo == null;
 	}
 
 	public void setCodigo(Long codigo) {
