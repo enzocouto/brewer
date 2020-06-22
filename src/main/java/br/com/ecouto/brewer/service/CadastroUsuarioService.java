636e71db81c484e5c63db646e0ad69221ce09d0f
Enzo Couto <enzo.couto@quod.com.br>
2020-06-22 13:24:17 -0300

    log senha

--- src/main/java/br/com/ecouto/brewer/service/CadastroUsuarioService.java
+++ src/main/java/br/com/ecouto/brewer/service/CadastroUsuarioService.java
@@ -2,12 +2,15 @@ package br.com.ecouto.brewer.service;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import br.com.ecouto.brewer.controller.CervejasController;
 import br.com.ecouto.brewer.model.Usuario;
 import br.com.ecouto.brewer.repository.UsuarioRepository;
 import br.com.ecouto.brewer.repository.VendasRepository;
@@ -26,6 +29,10 @@ public class CadastroUsuarioService {
 	@Autowired
 	VendasRepository vendaRepository;
 	
+	
+	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+	
+	
 	@Transactional
 	public void salvar(Usuario usuario) {
 		Optional<Usuario> usuarioExistente = repository.findByEmail(usuario.getEmail());
@@ -38,7 +45,9 @@ public class CadastroUsuarioService {
 		}
 		
 		if(usuario.isNovo() || !StringUtils.isEmpty(usuario.getSenha())) {
-			usuario.setSenha(this.passwordEncoder.encode(usuario.getSenha()));
+			String senha = this.passwordEncoder.encode(usuario.getSenha());
+			logger.info("criado password "+senha+ "usuario: "+usuario.getEmail());
+			usuario.setSenha(senha);
 		}else if(StringUtils.isEmpty(usuario.getSenha())) {
 			usuario.setSenha(usuarioExistente.get().getSenha());
 		}
@@ -63,4 +72,5 @@ public class CadastroUsuarioService {
 		repository.save(usuario);
 		
 	}
+	
 }
